3b7919e293e4c348189dfbb0a051705d69e6ce98
AvaniGupta17 <avanig142@gmail.com>
2025-02-11 22:57:56 +0530

    Create landing.jsx

---
+++ src/pages/landing.jsx
@@ -0,0 +1,71 @@
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Link } from "react-router-dom";
+import { Accordion, AccordionItem, AccordionTrigger, AccordionContent } from "@/components/ui/accordion";
+import faqs from "../data/faq.json";
+const LandingPage = () => {
+  return (
+    <main className="flex flex-col gap-10 sm:gap-20 py-10 sm:py-20">
+      <section className="text-center">
+        <h1 className="flex flex-col items-center justify-center gradient-title font-extrabold text-4xl sm:text-6xl lg:text-8xl tracking-tighter py-4">
+          Find Your Dream Job
+          <span className="flex items-center gap-2 sm:gap-6">
+            and get
+            <img
+              src="/logo.png"
+              className="h-14 sm:h-24 lg:h-32"
+              alt="PlaceD Logo"
+            />
+          </span>
+        </h1>
+        <p className="text-gray-300 sm:mt-4 text-xs sm:text-xl">
+          Explore thousands of job listings or find the perfect candidate
+        </p>
+        <br />
+        <div className="flex gap-6 justify-center mb-60">
+          <Link to={"/jobs"}>
+            <Button variant="blue" size="xl">
+              Find Jobs
+            </Button>
+          </Link>
+          <Link to={"/post-job"}>
+            <Button variant="destructive" size="xl">
+              Post a Job
+            </Button>
+          </Link>
+        </div>
+        <img src="/banner.jpeg" className="w-full" />
+      </section>
+
+      <section className="grid grid-cols-1 md:grid-cols-2 gap-4">
+        <Card>
+          <CardHeader>
+            <CardTitle>For Job Seekers</CardTitle>
+          </CardHeader>
+          <CardContent>
+            Search and apply for jobs, track applications, and more.
+          </CardContent>
+        </Card>
+        <Card>
+          <CardHeader>
+            <CardTitle>For Employers</CardTitle>
+          </CardHeader>
+          <CardContent>
+            Post jobs, manage applications, and find the best candidates.
+          </CardContent>
+        </Card>
+      </section>
+
+      <Accordion type="single" collapsible>
+        {faqs.map((faq, index) => (
+          <AccordionItem key={index} value={`item-${index + 1}`}>
+            <AccordionTrigger>{faq.question}</AccordionTrigger>
+            <AccordionContent>{faq.answer}</AccordionContent>
+          </AccordionItem>
+        ))}
+      </Accordion>
+    </main>
+  );
+};
+
+export default LandingPage;
