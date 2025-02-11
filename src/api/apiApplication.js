e710cb7e932050491a0d1cdda69ad631054aaff9
AvaniGupta17 <avanig142@gmail.com>
2025-02-11 22:47:25 +0530

    Create apiApplication.js

---
+++ src/api/apiApplication.js
@@ -0,0 +1,36 @@
+import supabaseClient, { supabaseUrl } from "@/utils/supabase";
+
+
+export async function applyToJob(token,  jobData) {
+  const supabase = await supabaseClient(token);
+
+  const random = Math.floor(Math.random() * 90000);
+  const fileName = `resume-${random}-${jobData.candidate_id}`;
+  //This generates a random number between 0 and 89999 and appends
+  //  it to the candidate's candidate_id to create a unique file name for the resume.
+
+  const { error: storageError } = await supabase.storage
+    .from("resumes")
+    .upload(fileName, jobData.resume);
+
+  if (storageError) throw new Error("Error uploading Resume");
+   
+const resume = `${spabaseUrl}/storage/v1/object/public/resumes/${fileName}`;
+
+  const { data, error } = await supabase
+    .from("applications")
+    .insert([
+      {
+        ...jobData,
+        resume,
+      },
+    ])
+    .select();
+
+  if (error) {
+    console.error(error);
+    throw new Error("Error submitting Application");
+  }
+
+  return data;
+}
